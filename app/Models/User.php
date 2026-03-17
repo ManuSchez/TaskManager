27c336121ffa2d2605712870becf4503cb6bd919
Manuel Sánchez <manuelsschez@gmail.com>
2026-03-17 21:53:19 +0100

    Solución de seguridad multiusuario, diseño móvil del sidebar y ruta del avatar por defecto

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,7 +71,7 @@ class User extends Authenticatable
     {
         return $this->avatar
             ? asset($this->avatar)
-            : asset('images/avatares/avatar1.png');
+            : asset('images/avatares/Avatar1.png');
     }
 
     /**
